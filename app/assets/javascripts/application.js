bd22351f28eaca935ceb841cdf81e480d4cb0bf4
H.Saing <hsaing@aol.com>
2013-10-05 18:52:45 -0700

    Add jQuery masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= jquery.masonry.min.js
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,16 @@
 class Pin < ActiveRecord::Base
+	validates :name, presence: true
 	validates :description, presence: true
-
-	belongs_to :user
 	validates :user_id, presence: true
+	# validates_attachment :image, presence: true,
+	# 							 content_type: { content_type: ['image/jpeg', 'image/png', 'image/jpg', 'image/gif'] },
+	# 							 size: { less_than: 5.megabytes }
+
+	# validates_attachment  :image, :presence => true,
+	# 					  :content_type => { :content_type => "image/jpg", "image/jpeg", "image/png", "image/git"  },
+	# 					  :size => { :in => 0..10.kilobytes }
+							     
+	belongs_to :user						      
+	has_attached_file :image, styles: { medium: "320x240>"}, :default_url => "/images/:style/missing.png"
 
 end
